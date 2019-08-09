409ce6cc23538cc2e8c3aa5c8c8d8d84c74b3b9f
stanislaw-s <useformyself1328@gmail.com>
2019-08-09 17:13:32 +0300

    Init app

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'Frontend\MainController@indexAction');
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
